993e4f5616c9f08a30a3f6b743c38ea425826bd5
Procyon_lotor123 <lotor@exolix.com>
2022-08-01 09:51:53 +0300

    fix

--- public_html/includes/head.php
+++ public_html/includes/head.php
@@ -18,6 +18,7 @@
 <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 <script>
     function onSubmit(token) {
+        console.log(token);
         document.getElementById("mail-send-form").submit();
     }
 </script>
